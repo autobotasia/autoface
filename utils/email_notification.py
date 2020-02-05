a612be3f25e2a12f80388458663a832c24bfa46d
Nguyen Tran Nhan <pocolomos@gmail.com>
2020-02-05 19:04:58 +0700

    WIP: update

--- utils/email_notification.py
+++ utils/email_notification.py
@@ -10,18 +10,15 @@ class Notification():
     def __init__(self, config):
         self.config = config
         password = config.sender_passwd
-        self.smtp_server = pjconfig.server
+        self.smtp_server = config.server
         context = ssl.create_default_context()
-        try:
-            self.smtp = smtplib.SMTP(smtp_server, 587)
-            self.smtp.ehlo()
-            self.smtp.starttls(context=context)
-            self.smtp.ehlo()
-            print('Start login', sender_email)
-            self.smtp.login(sender_email, password)
-            print('Login successfully.')
-        except Exception as e:
-            print(e)
+        self.smtp = smtplib.SMTP(smtp_server, 587)
+        self.smtp.ehlo()
+        self.smtp.starttls(context=context)
+        self.smtp.ehlo()
+        print('Start login', sender_email)
+        self.smtp.login(sender_email, password)
+        print('Login successfully.')
 
     def is_email(self, str_arg):
         regex = r'^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
@@ -30,12 +27,7 @@ class Notification():
         return False
 
     def logout(self):
-        try:
-            self.smtp.quit()
-            print('Logout successfully.')
-        except Exception as e:
-            print(e)
-
+        self.smtp.quit()
 
     def create_mess_with_attachment(self, frame, cls, prob):
         subject = cls + " đã đến lúc " + str(datetime.now())
@@ -49,28 +41,14 @@ class Notification():
         return message
 
 
-    def send_mail(sender_email, receiver_email, message):
-        try:
-            print('Start send mail from', sender_email, "to", receiver_email)
-            self.smtp.sendmail(sender_email, receiver_email, message.as_string())
-            print('Successfully send mail from', sender_email, "to", receiver_email + '.')
-        except Exception as e:
-            print(e)
-
-
-    def create_checkin_dict():
-        classname = {}
-        for _, clsdirs, _ in os.walk('./datasets/nccfaces/train/'):
-            for index, clsdir in enumerate(clsdirs):
-                classname[clsdir] = False
-        return classname
+    def send_mail(self, frame, cls, prob):
+        print('Start send mail from', self.config.sender_email, "to", self.config.receiver_email)
+        message = self.create_mess_with_attachment(frame, cls, prob)
+        self.smtp.sendmail(sender_email, receiver_email, message.as_string())
+        print('Successfully send mail from', sender_email, "to", receiver_email + '.')
 
 
     def is_new_day(self, saved_day):
         if date.today() != saved_day:
             return True
         return False
-
-    def send(self, frame, pred_clsname):
-        message = emailNotification.createMess(pred_clsname, sender_email, receiver_email)
-        emailNotification.sendMail(serverMail, sender_email, receiver_email, message)    
--- utils/mongodb.py
+++ utils/mongodb.py
@@ -1,6 +1,7 @@
 import pymongo
 from datetime import datetime
 import cv2
+from urllib.parse import quote_plus
 from bunch import Bunch
 import Notification
 
@@ -10,78 +11,25 @@ class AutofacesMongoDB():
         '''
         __init__(self, username, password, host, port, db = None, col = None)
         '''
-        try:
-            # Python 3.x
-            from urllib.parse import quote_plus
-        except ImportError:
-            # Python 2.x
-            from urllib import quote_plus
-
-        # mongodb_uri = 'mongodb://username:password@host:port/database?authSource=database&w=1'
-        # mongodb_uri = "mongodb://%s:%s@%s:%s/%s?authSource=%s&w=1"%(quote_plus(username), quote_plus(password), host, port, db, db)
-        print("ver 0.1, without authentication.")
-
-
         # MongoDB info
         dbconfig = Bunch(config.mongodb)
-        username = dbconfig.username
-        password = dbconfig.password
-        host = dbconfig.host
-        port = dbconfig.port
-        database_name = dbconfig.name
-        collection_name = dbconfig.colname
 
-        mongodb_uri = "mongodb://%s:%s"%( host, port)
-        # print("Mongo URI:", mongodb_uri)
+        self.host = dbconfig.host
+        self.port = dbconfig.port
+        self.db = dbconfig.name
+        self.collection = dbconfig.collection
+
+        mongodb_uri = "mongodb://%s:%s"%(self.host, self.port)
         
-        self.host = host
-        self.port = port
         print("Connecting to MongoDB...")
-        try:
-            self.client = pymongo.MongoClient(mongodb_uri)
-        except pymongo.errors.ConnectionFailure as e:
-            print("Could not connect to server: %s" % e)
-        print("Mongo connected.")
-        print("MongoDB host: %s" % self.host)
-        print("MongoDB port: %s" % self.port)
-        self.db = db
-        self.collection = col
-        print("Default database: %s" % self.db)
-        print("Default collection: %s" % self.collection)
-
+        self.client = pymongo.MongoClient(mongodb_uri)
         self.notifier = Notification(Bunch(config.notification))
-
-        # variable for email notification
-        # checkin dict save member checkin status, saved_day for check isNewDay
-        checkin = emailNotification.createCheckinDict()
-        saved_day = date.today()
 
     def dbinfo(self):
         print("MongoDB host: %s" % self.host)
         print("MongoDB port: %s" % self.port)
         print("Default database: %s" % self.db)
         print("Default collection: %s" % self.collection)
-
-
-    def get_client(self):
-        return self.client
-
-
-    def set_database(self, db):
-        '''
-        set objects' default database
-        '''
-        self.db = db
-        print("Default database: %s" % db)
-
-
-    def set_collection(self, col):
-        '''
-        set objects' default collection
-        '''
-        self.collection = col
-        print("Default collection %s" % col)
-
 
     def save(self, data, db = None, col = None):
         '''
@@ -145,4 +93,4 @@ class AutofacesMongoDB():
         
         if checkin[pred_clsname] is False:
             checkin[pred_clsname] = True
-            self.notifier.send(frame, pred_clsname, prob)
+            self.notifier.send_mail(frame, pred_clsname, prob)
