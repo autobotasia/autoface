f30fcdda67d1adab5dab40e52adfaf091ef1d2c4
Neil-Do <dotatthanhk60@gmail.com>
2020-06-14 07:20:59 +0700

    update.

--- tools/project/user_views.py
+++ tools/project/user_views.py
@@ -15,28 +15,16 @@ def get_current_user_profile(request):
 
     if request.user.is_authenticated:
         current_user = request.user
-        # context = model_to_dict(current_user)
-        # UserFormSet = modelformset_factory(User, fields=('last_login', 'username', "first_name", "last_name", "email", "date_joined"))
-        form = UserChangeForm(instance=current_user)
-        for k in form.fields.keys():
-            field = form.fields[k]
-            css_addition = ' form-control '
-            css_classes = field.widget.attrs.get('class', '')
-            field.widget.attrs['class'] = css_addition + css_classes
-            if k == 'last_login' or k == 'email' or k == 'date_joined':
-                field.widget.attrs['readonly'] = True
-                field.widget.attrs['style'] = 'background-color: #152036'
-
+        groups = []
+        for group in current_user.groups.all():
+            groups.append(str(group))
+        groups = ", ".join(groups)
 
         if request.method == 'POST':
-            form = UserChangeForm(request.POST, instance=current_user)
+            form = UserProfileForm(request.POST, instance=current_user)
             for k in form.fields.keys():
                 field = form.fields[k]
                 css_addition = ' form-control '
-                if k == 'last_login' or k == 'email' or k == 'date_joined':
-                    field.widget.attrs['readonly'] = True
-                    field.widget.attrs['style'] = 'background-color: #152036'
-
                 css_classes = field.widget.attrs.get('class', '')
                 field.widget.attrs['class'] = css_addition + css_classes
 
@@ -45,6 +33,16 @@ def get_current_user_profile(request):
                 messages.success(request, 'Updated profile')
                 return HttpResponseRedirect(request.path_info)
 
-        return render(request, 'user-profile.html', {'form': form})
+        # GET Method
+        form = UserProfileForm(instance=current_user)
+        user_data = { "is_staff" : current_user.is_staff, "is_active" : current_user.is_active, "is_superuser" : current_user.is_superuser, "last_login" : current_user.last_login, "date_joined" : current_user.date_joined, "email" : current_user.email, "groups" : groups }
+        #
+        for k in form.fields.keys():
+            field = form.fields[k]
+            css_addition = ' form-control '
+            css_classes = field.widget.attrs.get('class', '')
+            field.widget.attrs['class'] = css_addition + css_classes
+
+        return render(request, 'user-profile.html', { 'user_data': user_data, 'form': form})
 
     return HttpResponseRedirect('refuse_access')
